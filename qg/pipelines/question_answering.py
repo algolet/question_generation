bf229eac0ce6a0f75f2087409b41c2dc3d1de464
Vivicai1005 <caiweiwei1005@gmail.com>
2021-12-10 16:24:57 +0800

    edit qg and qa pipeline

--- qg/pipelines/question_answering.py
+++ qg/pipelines/question_answering.py
@@ -1,9 +1,19 @@
 from datasets import Dataset
 import logging
-import tensorflow as tf
-from qg.pipelines.utils_qa import postprocess_qa_predictions
+import torch
+from .utils_qa import postprocess_qa_predictions
+from typing import Union, List, Dict
 
 logger = logging.getLogger(__name__)
+
+if torch.cuda.is_available():
+    # Tell PyTorch to use the GPU.
+    device = torch.device("cuda")
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
 
 
 class QuestionAnsweringPipeline:
@@ -28,19 +38,20 @@ class QuestionAnsweringPipeline:
         self.n_best_size = n_best_size
         self.max_answer_length = max_answer_length
         self.null_score_diff_threshold = null_score_diff_threshold
-        self.per_device_eval_batch_size = 128
-        gpus = tf.config.list_physical_devices('GPU')
-        if len(gpus) == 0:
-            self.strategy = tf.distribute.OneDeviceStrategy(device="/cpu:0")
-        elif len(gpus) == 1:
-            self.strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
-        elif len(gpus) > 1:
-            self.strategy = tf.distribute.MirroredStrategy()
-        else:
-            raise ValueError("Cannot find the proper strategy, please check your environment properties.")
+        self.device = device
+        self.model = self.model.to(self.device)
+        self.model.eval()
+        # Validation preprocessing
 
     def prepare_validation_features(self, examples):
+        # Some of the questions have lots of whitespace on the left, which is not useful and will make the
+        # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
+        # left whitespace
         examples[self.question_column_name] = [q.lstrip() for q in examples[self.question_column_name]]
+
+        # Tokenize our examples with truncation and maybe padding, but keep the overflows using a stride. This results
+        # in one example possible giving several features when a context is long, each of those features having a
+        # context that overlaps a bit the context of the previous feature.
         tokenized_examples = self.tokenizer(
             examples[self.question_column_name],
             examples[self.context_column_name],
@@ -52,30 +63,35 @@ class QuestionAnsweringPipeline:
             padding="max_length" if self.pad_to_max_length else False
         )
 
+        # Since one example might give us several features if it has a long context, we need a map from a feature to
+        # its corresponding example. This key gives us just that.
         sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+
+        # For evaluation, we will need to convert our predictions to substrings of the context, so we keep the
+        # correspnding example_id and we will store the offset mappings.
         tokenized_examples["example_id"] = []
 
         for i in range(len(tokenized_examples["input_ids"])):
+            # Grab the sequence corresponding to that example (to know what is the context and what is the question).
             sequence_ids = tokenized_examples.sequence_ids(i)
             context_index = 1
 
+            # One example can give several spans, this is the index of the example containing this span of text.
             sample_index = sample_mapping[i]
             tokenized_examples["example_id"].append(examples["id"][sample_index])
 
+            # Set to None the offset_mapping that are not part of the context so it's easy to determine if a token
+            # position is part of the context or not.
             tokenized_examples["offset_mapping"][i] = [
                 (o if sequence_ids[k] == context_index else None)
                 for k, o in enumerate(tokenized_examples["offset_mapping"][i])
             ]
         return tokenized_examples
 
-    def preprocess(self, examples):
-        dataset = examples.map(
-            self.prepare_validation_features,
-            batched=True
-        )
-        return dataset
+        # region Metrics and Post-processing:
 
     def post_processing_function(self, examples, features, predictions, stage="eval"):
+        # Post-processing: we match the start logits and end logits to answers in the original context.
         predictions = postprocess_qa_predictions(
             examples=examples,
             features=features,
@@ -86,18 +102,31 @@ class QuestionAnsweringPipeline:
             null_score_diff_threshold=self.null_score_diff_threshold,
             prefix=stage,
         )
+        # Format the result to the format the metric expects.
         formatted_predictions = [{"id": k, "prediction": v} for k, v in predictions.items()]
         return formatted_predictions
 
-    def __call__(self, examples, *args, **kwargs):
-        logger.info("*** Prediction ***")
+    def __call__(self, input: Union[Dict, List[Dict]], *args, **kwargs):
+        if isinstance(input, dict):
+            questions = [input["question"]]
+            contexts = [input["context"]]
+        else:
+            questions = [item["question"] for item in input]
+            contexts = [item["context"] for item in input]
+        ids = [i for i in range(len(questions))]
+        examples = {"id": ids, "question": questions, "context": contexts}
         processed_datasets = self.prepare_validation_features(examples)
-        eval_inputs = {
-            "input_ids": tf.ragged.constant(processed_datasets["input_ids"]).to_tensor(),
-            "attention_mask": tf.ragged.constant(processed_datasets["attention_mask"]).to_tensor(),
+        inputs = {
+            "input_ids": torch.tensor(processed_datasets["input_ids"]),
+            "attention_mask": torch.tensor(processed_datasets["attention_mask"]),
         }
-        with self.strategy.scope():
-            eval_predictions = self.model.predict(eval_inputs, batch_size=self.per_device_eval_batch_size)
+
+        input_ids = inputs["input_ids"].to(self.device)
+        input_masks = inputs["attention_mask"].to(self.device)
+
+        with torch.no_grad():
+            eval_predictions = self.model(input_ids=input_ids,
+                                          attention_mask=input_masks)
         examples = Dataset.from_dict(examples)
         flat_examples = []
         for idx in range(len(examples["id"])):
@@ -112,10 +141,23 @@ class QuestionAnsweringPipeline:
                                             "attention_mask": processed_datasets["attention_mask"][idx],
                                             "offset_mapping": processed_datasets["offset_mapping"][idx],
                                             "example_id": processed_datasets["example_id"][idx]})
-
         post_processed_eval = self.post_processing_function(
             flat_examples,
             flat_processed_datasets,
-            (eval_predictions.start_logits, eval_predictions.end_logits),
+            (eval_predictions.start_logits.cpu().numpy(), eval_predictions.end_logits.cpu().numpy()),
         )
-        return post_processed_eval
+        res = []
+        for item in post_processed_eval:
+            if not item["prediction"]:
+                res.append(dict())
+            else:
+                if item["prediction"]["start"] == item["prediction"]["end"]:
+                    res.append(dict())
+                else:
+                    res.append({"text": item["prediction"]["text"],
+                                "start": item["prediction"]["start"],
+                                "end": item["prediction"]["end"],
+                                "score": item["prediction"]["probability"]})
+        if isinstance(input, dict):
+            return res[0]
+        return res
--- qg/pipelines/question_generation.py
+++ qg/pipelines/question_generation.py
@@ -1,8 +1,16 @@
 import logging
-from typing import List, Dict
+from typing import Union, List, Dict
 from torch import Tensor
 import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+if torch.cuda.is_available():
+    # Tell PyTorch to use the GPU.
+    device = torch.device("cuda")
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
 
 logger = logging.getLogger(__name__)
 
@@ -15,13 +23,19 @@ class QuestionGenerationPipeline:
                  max_target_length: int = 200):
         self.tokenizer = tokenizer
         self.model = model
+        self.device = device
+        self.model = self.model.to(self.device)
+        self.model.eval()
         self.max_source_length = max_source_length
         self.max_target_length = max_target_length
         self.source_prefix = "question generation: "
 
     def preprocess(self, examples: List[str]) -> Dict[str, Tensor]:
         added_prefix = ["question generation: " + example for example in examples]
-        inputs = self.tokenizer(added_prefix, return_tensors='pt', padding=True, truncation=True,
+        inputs = self.tokenizer(added_prefix,
+                                return_tensors='pt',
+                                padding=True,
+                                truncation=True,
                                 max_length=self.max_source_length)
         return inputs
 
@@ -36,25 +50,24 @@ class QuestionGenerationPipeline:
 
         return separated_questions
 
-    def __call__(self, paragraphs: List[str], *args, **kwargs):
+    def __call__(self, input: Union[str, List[str]], *args, **kwargs):
         logger.info("*** Prediction ***")
-        inputs = self.preprocess(paragraphs)
+        if isinstance(input, str):
+            examples = [input]
+        else:
+            examples = input
+        examples = self.preprocess(examples)
+        input_ids = examples["input_ids"].to(self.device)
+        input_mask = examples["attention_mask"].to(self.device)
+
         with torch.no_grad():
-            outs = self.model.generate(input_ids=inputs["input_ids"],
-                                       attention_mask=inputs["attention_mask"],
+            outs = self.model.generate(input_ids=input_ids,
+                                       attention_mask=input_mask,
                                        max_length=self.max_target_length,
                                        no_repeat_ngram_size=4,
-                                       num_beams=4
-                                       )
+                                       num_beams=4)
+
         post_processed = self.post_processing_function(outs)
-
+        if isinstance(input, str):
+            post_processed = post_processed[0]
         return post_processed
-
-
-if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained("/mnt/nfs/algo/models/mt5-small-zh-qg")
-    model = AutoModelForSeq2SeqLM.from_pretrained("/mnt/nfs/algo/models/mt5-small-zh-qg")
-    pipe = QuestionGenerationPipeline(tokenizer=tokenizer, model=model)
-    texts = ["格里戈里·米哈伊洛维奇·科津采夫（，拉丁化：Grigori Mikhailovich Kozintsev；，苏联电影导演，善用无声、蒙太奇手法，风格偏向表现主义。格里戈里·科津采夫在1905年3月22日生于（俄罗斯）罗曼诺夫王朝基辅，在圣彼得堡的帝国艺术学院就读，1921年开始制作电影。他执导的改编电影，包括威廉·莎士比亚的《李尔王》、《哈姆雷特》、米格尔·德·塞万提斯的《唐吉诃德》，都颇有名声。其中1964年的《哈姆雷特》是他的执导代表作，主演的因诺肯季·斯莫克图诺夫斯基凭此电影声名大噪。1973年5月11日，格里戈里·科津采夫在列宁格勒逝世。"]
-    question_res = pipe(texts)
-    print(question_res)
